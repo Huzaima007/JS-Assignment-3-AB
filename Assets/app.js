0c3984d5301de6749214f81110c0ec30eece939b
Huzaima007 <huzaimasiddiq17@gmail.com>
2024-10-20 19:27:32 +0500

    Update app.js

--- Assets/app.js
+++ Assets/app.js
@@ -10,15 +10,87 @@
 // alert("Hello, " + fullName + "! Welcome!");
 
 // Question 2
+
+// var favoritePhone = prompt("Enter your favorite mobile phone model:");
+// var lengthOfInput = favoritePhone.length;
+// console.log("The length of your input is: " + lengthOfInput);
+
 // Question 3
+
+// var word = "Pakistani";
+// var indexOfN = word.indexOf("n");
+// console.log("The index of 'n' in the word 'Pakistani' is: " + indexOfN);
+
 // Question 4
+
+// var phrase = "Hello World";
+// var lastIndexOfL = phrase.lastIndexOf("l");
+// console.log("The last index of 'l' in the phrase 'Hello World' is: " + lastIndexOfL);
+
 // Question 5
+
+// var word = "Pakistani";
+// var charAtThirdIndex = word.charAt(3);
+// console.log("The character at the 3rd index in the word 'Pakistani' is: " + charAtThirdIndex);
+
 // Question 6
+
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+// var fullName = firstName.concat(" ", lastName);
+// console.log("Hello, " + fullName + "!");
+
 // Question 7
+
+// var city = "Hyderabad";
+// var newCity = city.replace("Hyder", "Islam");
+// console.log("The new word is: " + newCity);
+
 // Question 8
+
+// var message =
+//   "Ali and Sami are best friends. They play cricket and football together.";
+// var updatedMessage = message.replace(/and/g, "&");
+// console.log(updatedMessage);
+
 // Question 9
+
+// var strValue = "472";
+// var numValue = Number(strValue);
+// console.log(
+//   "Value as string: " + strValue + " (Type: " + typeof strValue + ")"
+// );
+// console.log(
+//   "Value as number: " + numValue + " (Type: " + typeof numValue + ")"
+// );
+
 // Question 10
+
+// var userInput = prompt("Enter some text:");
+// var upperCaseInput = userInput.toUpperCase();
+// console.log("Your input in capital letters is: " + upperCaseInput);
+
 // Question 11
+
+// var userInput = prompt("Enter some text:");
+// var titleCaseInput = "";
+// var capitalizeNext = true;
+// for (var i = 0; i < userInput.length; i++) {
+//   var char = userInput[i];
+//   if (char === " ") {
+//     titleCaseInput += char;
+//     capitalizeNext = true;
+//   } else {
+//     if (capitalizeNext) {
+//       titleCaseInput += char.toUpperCase();
+//       capitalizeNext = false; // Reset the flag
+//     } else {
+//       titleCaseInput += char.toLowerCase();
+//     }
+//   }
+// }
+// console.log("Your input in title case is: " + titleCaseInput);
+
 // Question 12
 // Question 13
 // Question 14
