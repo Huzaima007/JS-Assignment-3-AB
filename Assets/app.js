3fafb0fd1e1774cfd9e003144eaa103a80668cf7
Huzaima007 <huzaimasiddiq17@gmail.com>
2024-10-20 13:27:37 +0500

    Create app.js

---
+++ Assets/app.js
@@ -0,0 +1,28 @@
+// ------------------------------------------------------------START------------------------------------------------------------
+
+// Chapter 21 To 25
+
+// Question 1
+
+// var firstName = prompt("Please enter your first name:");
+// var lastName = prompt("Please enter your last name:");
+// var fullName = firstName + " " + lastName;
+// alert("Hello, " + fullName + "! Welcome!");
+
+// Question 2
+// Question 3
+// Question 4
+// Question 5
+// Question 6
+// Question 7
+// Question 8
+// Question 9
+// Question 10
+// Question 11
+// Question 12
+// Question 13
+// Question 14
+// Question 15
+// Question 16
+// Question 17
+// Question 18
